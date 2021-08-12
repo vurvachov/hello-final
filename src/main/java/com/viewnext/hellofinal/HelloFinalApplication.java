9c3fea7834dbf45f0c13fafe162b5c7280d14811
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2021-08-12 08:15:50 -0500

    Initial import

---
+++ src/main/java/com/viewnext/hellofinal/HelloFinalApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.hellofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.hellofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
